b900ddfce9c08479db89ec7d1a78fc8531184ecc
AhmadJahanzaib10 <jahanzaibahmad200@gmail.com>
2025-07-18 19:04:22 +0500

    hundred22

--- assets/js/guzzler-section.js
+++ assets/js/guzzler-section.js
@@ -1,24 +1,24 @@
-const images = document.querySelectorAll(".img-box img");
-window.addEventListener("resize",()=>{
-    if(window.innerWidth < 1024){
-        images.forEach((img)=>{
-            const mobImg = img.getAttribute("data-img-mobile");
-            img.src = mobImg;
-            img.classList.remove("img-fix");
-            img.classList.add("w-100");
-        })
-    }
-    else{
-        img.classList.add("img-fix");
-        img.classList.remove("w-100");
-    }
-})
+// const images = document.querySelectorAll(".img-box img");
+// window.addEventListener("resize",()=>{
+//     if(window.innerWidth < 1024){
+//         images.forEach((img)=>{
+//             const mobImg = img.getAttribute("data-img-mobile");
+//             img.src = mobImg;
+//             img.classList.remove("img-fix");
+//             img.classList.add("w-100");
+//         })
+//     }
+//     else{
+//         img.classList.add("img-fix");
+//         img.classList.remove("w-100");
+//     }
+// })
 
-if(window.innerWidth < 1024){
-    images.forEach((img)=>{
-        const mobImg = img.getAttribute("data-img-mobile");
-        img.src = mobImg;
-        img.classList.remove("img-fix");
-        img.classList.add("w-100");
-    })
-}
+// if(window.innerWidth < 1024){
+//     images.forEach((img)=>{
+//         const mobImg = img.getAttribute("data-img-mobile");
+//         img.src = mobImg;
+//         img.classList.remove("img-fix");
+//         img.classList.add("w-100");
+//     })
+// }
